7d56ade6ab49faead91c78587978934b6198b84a
shahadot99999 <shahadot99999@gmail.com>
2022-08-29 18:25:08 +0600

    first commit

---
+++ js/randmouse.js
@@ -0,0 +1,28 @@
+const loadUsers = () => {
+    fetch('https://randomuser.me/api/?results=10')
+        .then(res => res.json())
+        .then(data => displayUsers(data.results))
+}
+
+const displayUsers = users => {
+    const usersContainer = document.getElementById('users-container');
+    for (const user of users) {
+        console.log(user);
+        const userDiv = document.createElement('div');
+        userDiv.classList.add('user');
+        userDiv.innerHTML = `
+            <h3>User Name: ${user.name.title} ${user.name.first} ${user.name.last}</h3>
+            <img src="${user.picture.large}"  alt="...">
+            <p>User Location: ${user.location.city} ${user.location.country}</p>
+            <p>User coordinates: ${user.location.coordinates.latitude}</p>
+            <p>User timezone: ${user.location.timezone.description}</p>
+            
+           
+
+        `;
+        usersContainer.appendChild(userDiv);
+    }
+}
+// <p>User timezone: ${user.location[3].timezone}</p>
+//<p>User coordinates: ${user.location[1].coordinates}</p>
+loadUsers();
